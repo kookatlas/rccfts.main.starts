e3f8e5b802c1db395217c14ea7a11cf492f93804
srinivas yadav <seenugadu225@gmail.com>
2026-01-18 21:35:24 +0530

    my first commit

---
+++ src/main/java/in/koost/rccfts/main/starts/Application.java
@@ -0,0 +1,13 @@
+package in.koost.rccfts.main.starts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/koost/rccfts/main/starts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.koost.rccfts.main.starts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
